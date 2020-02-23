00173257a4fad080c5981a08d24337f93b06962b
Shivam <utpal.shivam@accenture.com>
2020-02-23 20:13:34 +0530

    test

--- src/p1/DragDropTouchAction.java
+++ src/p1/DragDropTouchAction.java
@@ -11,12 +11,14 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.android.nativekey.AndroidKey;
+import io.appium.java_client.android.nativekey.KeyEvent;
 import io.appium.java_client.remote.MobileCapabilityType;
 import io.appium.java_client.touch.LongPressOptions;
 import io.appium.java_client.touch.TapOptions;
 import io.appium.java_client.touch.offset.ElementOption;
 
-public class ScrollingTouchAction {
+public class DragDropTouchAction {
 
 	public static void main(String[] args) throws MalformedURLException, InterruptedException {
 
@@ -30,8 +32,20 @@ public class ScrollingTouchAction {
 		AndroidDriver<AndroidElement> driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), cap);
 		// views--Expandable list--Custom Adaptor--People names
 		driver.findElementByXPath("//android.widget.TextView[@text='Views']").click();
+		driver.findElementByXPath("//android.widget.TextView[@text='Drag and Drop']").click();
 
-		driver.findElementByAndroidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"WebView\"));");
+		WebElement src = driver.findElementByXPath("//android.view.View[@index='1']");
+		WebElement des = driver.findElementByXPath("//android.view.View[@index='2']");
 
+		TouchAction t = new TouchAction(driver);
+		t.longPress(LongPressOptions.longPressOptions().withElement(ElementOption.element(src))
+				.withDuration(Duration.ofSeconds(2))).moveTo(ElementOption.element(des)).release().perform();
+
+		// Below also can be used
+		((AndroidDriver) driver).pressKey(new KeyEvent(AndroidKey.BACK));
+		driver.findElementByXPath("//android.widget.TextView[@text='Drag and Drop']").click();
+
+		Thread.sleep(2000);
+		t.longPress(ElementOption.element(src)).moveTo(ElementOption.element(des)).perform().release();//StaleElementException
 	}
 }
